3b579f75d46adbf66ac3cb7a2175f7cda9ab9780
Peter Huynh <Peter.Huynh@colorado.edu>
2017-05-06 13:46:43 -0600

    Octagon detection of width/height 40

--- catkin_ws/devel/share/pid/cmake/pidConfig.cmake
+++ catkin_ws/devel/share/pid/cmake/pidConfig.cmake
@@ -67,14 +67,14 @@ set(pid_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
-  set(pid_SOURCE_PREFIX /home/cody/Dropbox/csci4302_FinalProject/catkin_ws/src/pid)
-  set(pid_DEVEL_PREFIX /home/cody/Dropbox/csci4302_FinalProject/catkin_ws/devel)
+  set(pid_SOURCE_PREFIX /home/user/Desktop/csci4302_FinalProject/catkin_ws/src/pid)
+  set(pid_DEVEL_PREFIX /home/user/Desktop/csci4302_FinalProject/catkin_ws/devel)
   set(pid_INSTALL_PREFIX "")
   set(pid_PREFIX ${pid_DEVEL_PREFIX})
 else()
   set(pid_SOURCE_PREFIX "")
   set(pid_DEVEL_PREFIX "")
-  set(pid_INSTALL_PREFIX /home/cody/Dropbox/csci4302_FinalProject/catkin_ws/install)
+  set(pid_INSTALL_PREFIX /home/user/Desktop/csci4302_FinalProject/catkin_ws/install)
   set(pid_PREFIX ${pid_INSTALL_PREFIX})
 endif()
 
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(pid_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT "/home/cody/Dropbox/csci4302_FinalProject/catkin_ws/devel/include;/home/cody/Dropbox/csci4302_FinalProject/catkin_ws/src/pid/include " STREQUAL " ")
+if(NOT "/home/user/Desktop/csci4302_FinalProject/catkin_ws/devel/include;/home/user/Desktop/csci4302_FinalProject/catkin_ws/src/pid/include " STREQUAL " ")
   set(pid_INCLUDE_DIRS "")
-  set(_include_dirs "/home/cody/Dropbox/csci4302_FinalProject/catkin_ws/devel/include;/home/cody/Dropbox/csci4302_FinalProject/catkin_ws/src/pid/include")
+  set(_include_dirs "/home/user/Desktop/csci4302_FinalProject/catkin_ws/devel/include;/home/user/Desktop/csci4302_FinalProject/catkin_ws/src/pid/include")
   foreach(idir ${_include_dirs})
     if(IS_ABSOLUTE ${idir} AND IS_DIRECTORY ${idir})
       set(include ${idir})
@@ -103,7 +103,7 @@ if(NOT "/home/cody/Dropbox/csci4302_FinalProject/catkin_ws/devel/include;/home/c
         message(FATAL_ERROR "Project 'pid' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  Ask the maintainer 'Andy Zelenak <andyz@utexas.edu>' to fix it.")
       endif()
     else()
-      message(FATAL_ERROR "Project 'pid' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/cody/Dropbox/csci4302_FinalProject/catkin_ws/src/pid/${idir}'.  Ask the maintainer 'Andy Zelenak <andyz@utexas.edu>' to fix it.")
+      message(FATAL_ERROR "Project 'pid' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/user/Desktop/csci4302_FinalProject/catkin_ws/src/pid/${idir}'.  Ask the maintainer 'Andy Zelenak <andyz@utexas.edu>' to fix it.")
     endif()
     _list_append_unique(pid_INCLUDE_DIRS ${include})
   endforeach()
@@ -122,7 +122,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/cody/Dropbox/csci4302_FinalProject/catkin_ws/devel/lib;/home/cody/Dropbox/csci4302_FinalProject/catkin_ws/devel/lib;/home/cody/Dropbox/advanced_robotics/coco3090/Homework3/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/user/Desktop/csci4302_FinalProject/catkin_ws/devel/lib;/home/user/Desktop/csci4302_FinalProject/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
--- catkin_ws/src/stop_sign/scripts/shapeDetect.py
+++ catkin_ws/src/stop_sign/scripts/shapeDetect.py
@@ -5,28 +5,22 @@ import cv2
 print cv2.__version__
 
 img = cv2.imread('3.jpg')
-#gray = cv2.imread('shapes.png',0)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret,thresh = cv2.threshold(gray,127,255,0)
 im2,contours,h = cv2.findContours(thresh.copy(),1,2)
 
 for cnt in contours:
-    M = cv2.moments(cnt)
-    #print M
-    #cx = int(M['m10']/M['m00'])
-    #cy = int(M['m01']/M['m00'])
     area = cv2.contourArea(cnt)
     perimeter = cv2.arcLength(cnt,True)
     epsilon = 0.01*perimeter
     approx = cv2.approxPolyDP(cnt,epsilon,True)
-    hull = cv2.convexHull(cnt)
-    k = cv2.isContourConvex(cnt)
-    #print len(approx)
-    if len(approx) == 8:
-        #print "octagon"
-        cv2.drawContours(img,[cnt],0,(255,55,55),3)
+    x,y,w,h = cv2.boundingRect(cnt)
+    if len(approx) == 8 and w >= 40 and h >= 40:
+        #print area
+        cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.drawContours(gray,[cnt],0,(255,55,55),3)
 
-cv2.imshow('img',img)
+cv2.imshow('gray',gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
