f24cae1d920b192dbcdb2ac404a1b2910ac39c43
Peter Huynh <Peter.Huynh@colorado.edu>
2017-05-06 20:22:17 -0600

    I have no idea what I'm doing

--- catkin_ws/src/stop_sign/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Desktop/csci4302_FinalProject/catkin_ws/src/stop_sign
-# Build directory: /home/user/Desktop/csci4302_FinalProject/catkin_ws/src/stop_sign/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- catkin_ws/src/stop_sign/scripts/shapeDetect.py
+++ catkin_ws/src/stop_sign/scripts/shapeDetect.py
@@ -1,26 +1,61 @@
 #! /usr/bin/env python
+from std_msgs.msg import String
 import numpy as np
+import roslib
+roslib.load_manifest('my_package')
+import sys
+import rospy
 import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-print cv2.__version__
+from __future__ import print_function
 
-img = cv2.imread('3.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+class image_converter:
+	def __init__(self):
+		self.image_pub = rospy.Publisher("stop_sign",Image)
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("camera/image_raw",Image,self.callback)
 
-ret,thresh = cv2.threshold(gray,127,255,0)
-im2,contours,h = cv2.findContours(thresh.copy(),1,2)
+	def callback(self,data):
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+		(rows,cols,channels) = cv_image.shape
+		
+		ret,thresh = cv2.threshold(cv_image,127,255,0)
+		im2,contours,h = cv2.findContours(thresh.copy(),1,2)
+		
+		for cnt in contours:
+			area = cv2.contourArea(cnt)
+			perimeter = cv2.arcLength(cnt,True)
+			epsilon = 0.01*perimeter
+			approx = cv2.approxPolyDP(cnt,epsilon,True)
+			x,y,w,h = cv2.boundingRect(cnt)
+			if len(approx) == 8 and w >= 40 and h >= 40:
+				cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),2)
+				cv2.drawContours(gray,[cnt],0,(255,55,55),3)
+		
+		if cols > 60 and rows > 60 :
+			cv2.circle(cv_image, (50,50), 10, 255)
+		cv2.imshow("Image window", cv_image)
+		cv2.waitKey(3)
 
-for cnt in contours:
-    area = cv2.contourArea(cnt)
-    perimeter = cv2.arcLength(cnt,True)
-    epsilon = 0.01*perimeter
-    approx = cv2.approxPolyDP(cnt,epsilon,True)
-    x,y,w,h = cv2.boundingRect(cnt)
-    if len(approx) == 8 and w >= 40 and h >= 40:
-        #print area
-        cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),2)
-        cv2.drawContours(gray,[cnt],0,(255,55,55),3)
+		try:
+			self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+		except CvBridgeError as e:
+			print(e)
+	
+	def main(args):
+		ic = image_converter()
+		rospy.init_node('image_converter', anonymous=True)
+		try:
+			rospy.spin()
+		except KeyboardInterrupt:
+			print("Shutting down")
+		cv2.destroyAllWindows()
 
-cv2.imshow('gray',gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main(sys.argv)
